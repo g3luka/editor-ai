cbf1c47c3df14c035748e25750e5d9c6864057de
Giovanni de Luca <g3luka@gmail.com>
2025-05-13 20:59:42 -0300

    ci: Install, Build e Release

--- blocks/build/editor-ai/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-plugins', 'wp-primitives'), 'version' => 'e867390b0efd74404da3');
